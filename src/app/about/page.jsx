03cbd68d6166e84d9efea354cff3b6643a62c7b7
seohyon-cho <flanique@naver.com>
2024-01-15 16:12:39 +0900

    header 스타일링 완료

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 import styles from './about.module.scss';
 
 export default function About() {
-	throw new Error('Error in About Page...');
+	// throw new Error('Error in About Page...');
 	return (
 		<section className={clsx(styles.about)}>
 			<h1>About</h1>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
 				<Link href='/'>DCODELAB</Link>
 			</h1>
 			<Navbar textArr={['about', 'youtube', 'post']} />
-			<BtnLogin session={false} />
+			<BtnLogin session={true} />
 		</header>
 	);
 }
