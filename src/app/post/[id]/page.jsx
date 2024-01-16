3cb425554df9695e4898fb6abb3f5cc7ca6e5ee7
seohyon-cho <flanique@naver.com>
2024-01-16 16:34:10 +0900

    상세페이지 데이터 호출 SSR

--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -1,8 +1,12 @@
 import clsx from 'clsx';
 import styles from './postDetail.module.scss';
+import { getPosts } from '@/lib/actions';
 
-export default function PostDetail({ params }) {
-	const [id] = params;
+export default async function PostDetail({ params }) {
+	const { id } = params;
+	const post = await getPosts(id);
+	console.log(post);
+
 	return (
 		<section className={clsx(styles.postDetail)}>
 			<h1>PostDetail</h1>
