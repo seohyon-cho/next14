88693d54f73b87c9eeb52e6fbfd31d50848096ea
seohyon-cho <flanique@naver.com>
2024-01-16 16:41:53 +0900

    post 상세페이지 데이터 출력 및 스타일링

--- src/app/post/[id]/page.jsx
+++ src/app/post/[id]/page.jsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx';
 import styles from './postDetail.module.scss';
 import { getPosts } from '@/lib/actions';
+import Image from 'next/image';
 
 export default async function PostDetail({ params }) {
 	const { id } = params;
@@ -10,6 +11,14 @@ export default async function PostDetail({ params }) {
 	return (
 		<section className={clsx(styles.postDetail)}>
 			<h1>PostDetail</h1>
+
+			<article>
+				<div className={clsx(styles.pic)}>{post.img && <Image src={post.img} alt={post.title} priority fill />}</div>
+				<div className={clsx(styles.txt)}>
+					<h2>{post.title}</h2>
+					<p>{post.desc}</p>
+				</div>
+			</article>
 		</section>
 	);
 }
