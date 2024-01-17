1178d00a2fb3951fc0e8a93860af9f94b3e59559
seohyon-cho <flanique@naver.com>
2024-01-18 08:46:52 +0900

    기존 데이터를 가져와서 input에 담아 출력하는 수정 페이지 컴포넌트 추가

--- src/app/post/edit/[id]/page.jsx
+++ src/app/post/edit/[id]/page.jsx
@@ -1,12 +1,24 @@
 import clsx from 'clsx';
 import styles from './edit.module.scss';
+import { getPosts } from '@/lib/actions';
 
-export default function Edit({ params }) {
+export default async function Edit({ params }) {
 	const { id } = params;
+	const data = await getPosts(id);
 	return (
 		<section className={clsx(styles.edit)}>
 			<h1>edit</h1>
-			<p>{id}</p>
+			<form>
+				<input type='hidden' name={id} />
+				<input type='text' name='title' value={data.title} />
+				<input type='text' name='img' value={data.img || ''} />
+				<textarea name='desc' cols='30' rows='3' value={data.desc}></textarea>
+
+				<nav>
+					<input type='reset' value='cancel' />
+					<input type='submit' value='update' />
+				</nav>
+			</form>
 		</section>
 	);
 }
