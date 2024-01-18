6c4516d7fd601d899f76a9a325ba477c3815b672
seohyon-cho <flanique@naver.com>
2024-01-18 10:50:13 +0900

    InputImage컴포넌트에서 삼항연산자 조건을 data props가 아닌 ImgUrl값으로 활용하는 이유

--- src/components/inputImage/InputImage.jsx
+++ src/components/inputImage/InputImage.jsx
@@ -16,7 +16,12 @@ export default function InputImage({ data }) {
 			<input
 				type='text'
 				placeholder='image URL'
-				value={ImgUrl !== '' ? ImgUrl : data?.img || ''}
+				//ImgUrl(Flickr Panel에서 전달받는 값: 처음 포스트 작성시: Post> Write)
+				//data.img (이미작성된 상세페이지 데이터로 부터 전달 받는값: Post> Edit)
+				//ImgUrl값이 없으면 수정모드일 때이므로 data.img를 활용
+				//ImgUrl값이 있으면 처음 글 작성모드일 때이므로 ImgUrl을 활용
+				//value={ImgUrl !== '' ? ImgUrl : data?.img || ''}
+				value={ImgUrl ? ImgUrl : data?.img || ''}
 				name='img'
 				onChange={e => setImgUrl(e.target.value)}
 			/>
