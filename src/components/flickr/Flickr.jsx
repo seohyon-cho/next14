d27cbd5cb46f79793ccb046d35c920ef5536316d
seohyon-cho <flanique@naver.com>
2024-01-18 11:20:49 +0900

    flickr 썸네일 (추천이미지) 패널에서 키워드 검색해서 검색 이미지 찾을 수 있게끔 로직 구현

--- src/components/flickr/Flickr.jsx
+++ src/components/flickr/Flickr.jsx
@@ -2,26 +2,38 @@
 import clsx from 'clsx';
 import styles from './flickr.module.scss';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useGlobalData } from '@/hooks/useGlobalData';
 
 export default function Flickr() {
+	const inputEl = useRef(null);
+
 	const { ImgPanelOpen, setImgPanelOpen, setImgUrl } = useGlobalData();
 	const [Pics, setPics] = useState([]);
 
-	useEffect(() => {
-		const fetchFlickr = async () => {
-			const num = 18;
-			const flickr_api = process.env.NEXT_PUBLIC_FLICKR_API;
-			const baseURL = `https://www.flickr.com/services/rest/?&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1&method=`;
-			const method_interest = 'flickr.interestingness.getList';
-			const interestURL = `${baseURL}${method_interest}`;
-			const data = await fetch(interestURL);
-			const response = await data.json();
-			setPics(response.photos.photo);
-		};
+	const fetchFlickr = async opt => {
+		const num = 18;
+		const flickr_api = process.env.NEXT_PUBLIC_FLICKR_API;
+		const baseURL = `https://www.flickr.com/services/rest/?&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1&method=`;
+		const method_interest = 'flickr.interestingness.getList';
+		const method_search = 'flickr.photos.search';
+		let url = '';
+		if (opt.type === 'interest') url = `${baseURL}${method_interest}`;
+		if (opt.type === 'search') url = `${baseURL}${method_search}&tags=${opt.tag}`;
+		const data = await fetch(url);
+		const response = await data.json();
+		setPics(response.photos.photo);
+	};
 
-		fetchFlickr();
+	const handleSearch = e => {
+		e.preventDefault();
+		const tag = inputEl.current.value.trim();
+		if (!tag) return;
+		fetchFlickr({ type: 'search', tag: tag });
+	};
+
+	useEffect(() => {
+		fetchFlickr({ type: 'interest' });
 	}, []);
 
 	return (
@@ -30,6 +42,12 @@ export default function Flickr() {
 				<aside className={clsx(styles.unsplash)}>
 					<h1>Unsplash</h1>
 					<button onClick={() => setImgPanelOpen(false)}>close</button>
+
+					<div>
+						<input type='text' placeholder='search' ref={inputEl} />
+						<button onClick={handleSearch}>search</button>
+					</div>
+
 					<div>
 						{Pics.map((pic, idx) => {
 							return (
