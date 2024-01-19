59458c856cabed3877d0b1db0269e074d0778bfa
seohyon-cho <flanique@naver.com>
2024-01-19 13:19:44 +0900

    github 계정 로그인 시, 계정 정보 mongoDB로 연동되어 저장됨

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,25 +1,31 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
+import Github from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
+
 import { connectDB } from './connectDB';
 import { User } from './Models';
 import bcrypt from 'bcryptjs';
 import { authConfig } from './auth.config';
-import Github from 'next-auth/providers/github';
-import Google from 'next-auth/providers/google';
+
 //로그인정보 DB정보에서 찾아서 인증 함수
 const checkUserDB = async credentials => {
 	try {
 		connectDB();
+
 		const user = await User.findOne({ username: credentials.username });
 		if (!user) throw new Error('there is no username yout input in DB!!');
+
 		const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
 		if (!isPasswordCorrect) throw new Error('now matched password from DB!');
+
 		return user;
 	} catch (err) {
 		console.log(err);
 		throw new Error('Failed to login!');
 	}
 };
+
 //NextAuth의 리턴값을 바로 비구조화할당해서 export로 내보냄
 export const {
 	handlers: { GET, POST },
@@ -45,6 +51,7 @@ export const {
 			clientId: process.env.GITHUB_ID,
 			clientSecret: process.env.GITHUB_SECRET
 		}),
+		//google 인증 Provider설정
 		Google({
 			clientId: process.env.GOOGLE_CLIENT_ID || '',
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
@@ -53,6 +60,25 @@ export const {
 	//인증이 성공완료된 자동 실행될 callback함수(외부 autoConfig에서 가져옴)
 	callbacks: {
 		async signIn({ user, account, profile }) {
+			if (account.provider === 'github') {
+				connectDB();
+				try {
+					const user = await User.findOne({ email: profile.email });
+
+					if (!user) {
+						const newUser = new User({
+							username: profile.login,
+							email: profile.email,
+							img: profile.avatar_url
+						});
+
+						await newUser.save();
+					}
+				} catch (err) {
+					console.log(err);
+					return false;
+				}
+			}
 			if (account.provider === 'google') {
 				console.log('google', account);
 			}
