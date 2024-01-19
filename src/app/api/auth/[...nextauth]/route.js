64c3dd690ae3387ef6cd39e97074f06e1c284850
seohyon-cho <flanique@naver.com>
2024-01-19 09:17:18 +0900

    next13, 14서버 응답 차이 api router vs server action 차이 정리

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -16,3 +16,19 @@ export { GET, POST } from '@/lib/auth';
   -----------인증을 위한 모든 설정 완료-----------------
   
 */
+
+/*
+  DB데이터 입출력에 대한 next13 vs next14 차이점 정리
+
+  next13 (API Routing)
+  요청(fetch:GET, POST, PUT, DELETE) ---> 서버응답 (api/path/route.js)
+  -단점: 요청보낼때 fetch문에 params값을 지저분하게 달아서 요처
+  -단점: 서버응답시 요청에 맞게 일일 폴더를 새로 만들어야 되는 번거로움
+  
+  next14 (Server Action)
+  요청(form에 action에 서버쪽에 응답할 함수만 등록) ----> 서버응답 (해당파일안에서 action함수 추가)
+  -장점: 서버로 전달값을 일일이 state에 담고 fetching함수하고 useEffect안쪽에서 fetching보내는 번거로운 작업없이
+  그냥 form안쪽에 input항목마다 name값을 달기만 하면됨 (서버액션함수에 찰떡같이 name값을 키값으로 해서 모든값 받아줌)
+  -장점: 일일 api폴더 안쪽에 요청path에 맞게 폴더추가및 route.js파일 추가 안해도됨
+  그냥 요청컴포넌트 안쪽에 DB입출력해주는 함수 추가 formData props로 알아서 전달받은 받음
+*/
