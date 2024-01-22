09e73874853259ff24e055b359c542ddaa70cecd
seohyon-cho <flanique@naver.com>
2024-01-22 09:50:30 +0900

    글작성자 정보출력 흐름 주석 정리

--- src/app/post/write/page.jsx
+++ src/app/post/write/page.jsx
@@ -27,3 +27,13 @@ export default async function Write() {
 		</>
 	);
 }
+
+/*
+	포스트저장시 글작성자 정보 같이 저장하는 로직 흐름
+	1. app/post/write -> auth객체에서 로그인된 사용자정보인 session을 가져와서 email값을 input에 hidden숨겨서 addPost 서버액션함수로 전달
+	2. lib/actions.js (addPost) -> 파라미터로 email값을 받아서 email값 추가해서 모델 인스턴스 생성후 DB에 저장
+	3. app/post/[id] -> 상세페이지 접속시 getPosts 서버액션 함수로 상세포스트 정보 가져옴
+	4. <UserInfo /> -> 상세페이지 안쪽의 해당 컴포넌트의 email값을 props로 전달 (동기화를 위해 Suspense활용)
+	5. components > userInfo (getUser) -> getUser서버 액션함수가 email값을 전달받아서 해당 유저정보객체 반환
+	6. 반환된 유저정보를 userInfo컴포넌트에 원하는 형태로 출력
+*/
